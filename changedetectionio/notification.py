b5efdeb4e4e3215f04f0f8b45c700e4b1541395c
dt_4541218930 <abcstorms@163.com>
2025-01-15 13:19:13 +0800

    恢复apprise发送消息功能

--- changedetectionio/notification.py
+++ changedetectionio/notification.py
@@ -78,6 +78,7 @@ def process_notification(n_object, datastore):
         return None
 
     with apprise.LogCapture(level=apprise.logging.DEBUG) as logs:
+        need_apprise = False
         for url in n_object['notification_urls']:
 
             # # Get the notification body from datastore
@@ -85,92 +86,95 @@ def process_notification(n_object, datastore):
             if n_object.get('notification_format', '').startswith('HTML'):
                 n_body = n_body.replace("\n", '<br>')
             
-            notify_dingtalk(n_body, url)
+            n_title = jinja_render(template_str=n_object.get('notification_title', ''), **notification_parameters)
 
-            # n_title = jinja_render(template_str=n_object.get('notification_title', ''), **notification_parameters)
+            url = url.strip()
+            if url.startswith('#'):
+                logger.trace(f"Skipping commented out notification URL - {url}")
+                continue
 
-            # url = url.strip()
-            # if url.startswith('#'):
-            #     logger.trace(f"Skipping commented out notification URL - {url}")
-            #     continue
+            if not url:
+                logger.warning(f"Process Notification: skipping empty notification URL.")
+                continue
 
-            # if not url:
-            #     logger.warning(f"Process Notification: skipping empty notification URL.")
-            #     continue
+            logger.info(f">> Process Notification: AppRise notifying {url}")
+            url = jinja_render(template_str=url, **notification_parameters)
 
-            # logger.info(f">> Process Notification: AppRise notifying {url}")
-            # url = jinja_render(template_str=url, **notification_parameters)
+            if url.startswith('discord://') and url.find("oapi.dingtalk.com/robot/send"):
+                notify_dingtalk(n_body, url)
+                continue
+            # Re 323 - Limit discord length to their 2000 char limit total or it wont send.
+            # Because different notifications may require different pre-processing, run each sequentially :(
+            # 2000 bytes minus -
+            #     200 bytes for the overhead of the _entire_ json payload, 200 bytes for {tts, wait, content} etc headers
+            #     Length of URL - Incase they specify a longer custom avatar_url
 
-            # # Re 323 - Limit discord length to their 2000 char limit total or it wont send.
-            # # Because different notifications may require different pre-processing, run each sequentially :(
-            # # 2000 bytes minus -
-            # #     200 bytes for the overhead of the _entire_ json payload, 200 bytes for {tts, wait, content} etc headers
-            # #     Length of URL - Incase they specify a longer custom avatar_url
+            # So if no avatar_url is specified, add one so it can be correctly calculated into the total payload
+            k = '?' if not '?' in url else '&'
+            if not 'avatar_url' in url \
+                    and not url.startswith('mail') \
+                    and not url.startswith('post') \
+                    and not url.startswith('get') \
+                    and not url.startswith('delete') \
+                    and not url.startswith('put'):
+                url += k + 'avatar_url=https://raw.githubusercontent.com/dgtlmoon/changedetection.io/master/changedetectionio/static/images/avatar-256x256.png'
 
-            # # So if no avatar_url is specified, add one so it can be correctly calculated into the total payload
-            # k = '?' if not '?' in url else '&'
-            # if not 'avatar_url' in url \
-            #         and not url.startswith('mail') \
-            #         and not url.startswith('post') \
-            #         and not url.startswith('get') \
-            #         and not url.startswith('delete') \
-            #         and not url.startswith('put'):
-            #     url += k + 'avatar_url=https://raw.githubusercontent.com/dgtlmoon/changedetection.io/master/changedetectionio/static/images/avatar-256x256.png'
+            if url.startswith('tgram://'):
+                # Telegram only supports a limit subset of HTML, remove the '<br>' we place in.
+                # re https://github.com/dgtlmoon/changedetection.io/issues/555
+                # @todo re-use an existing library we have already imported to strip all non-allowed tags
+                n_body = n_body.replace('<br>', '\n')
+                n_body = n_body.replace('</br>', '\n')
+                # real limit is 4096, but minus some for extra metadata
+                payload_max_size = 3600
+                body_limit = max(0, payload_max_size - len(n_title))
+                n_title = n_title[0:payload_max_size]
+                n_body = n_body[0:body_limit]
 
-            # if url.startswith('tgram://'):
-            #     # Telegram only supports a limit subset of HTML, remove the '<br>' we place in.
-            #     # re https://github.com/dgtlmoon/changedetection.io/issues/555
-            #     # @todo re-use an existing library we have already imported to strip all non-allowed tags
-            #     n_body = n_body.replace('<br>', '\n')
-            #     n_body = n_body.replace('</br>', '\n')
-            #     # real limit is 4096, but minus some for extra metadata
-            #     payload_max_size = 3600
-            #     body_limit = max(0, payload_max_size - len(n_title))
-            #     n_title = n_title[0:payload_max_size]
-            #     n_body = n_body[0:body_limit]
+            elif url.startswith('discord://') or url.startswith('https://discordapp.com/api/webhooks') or url.startswith(
+                    'https://discord.com/api'):
+                # real limit is 2000, but minus some for extra metadata
+                payload_max_size = 1700
+                body_limit = max(0, payload_max_size - len(n_title))
+                n_title = n_title[0:payload_max_size]
+                n_body = n_body[0:body_limit]
 
-            # elif url.startswith('discord://') or url.startswith('https://discordapp.com/api/webhooks') or url.startswith(
-            #         'https://discord.com/api'):
-            #     # real limit is 2000, but minus some for extra metadata
-            #     payload_max_size = 1700
-            #     body_limit = max(0, payload_max_size - len(n_title))
-            #     n_title = n_title[0:payload_max_size]
-            #     n_body = n_body[0:body_limit]
+            elif url.startswith('mailto'):
+                # Apprise will default to HTML, so we need to override it
+                # So that whats' generated in n_body is in line with what is going to be sent.
+                # https://github.com/caronc/apprise/issues/633#issuecomment-1191449321
+                if not 'format=' in url and (n_format == 'Text' or n_format == 'Markdown'):
+                    prefix = '?' if not '?' in url else '&'
+                    # Apprise format is lowercase text https://github.com/caronc/apprise/issues/633
+                    n_format = n_format.lower()
+                    url = f"{url}{prefix}format={n_format}"
+                # If n_format == HTML, then apprise email should default to text/html and we should be sending HTML only
 
-            # elif url.startswith('mailto'):
-            #     # Apprise will default to HTML, so we need to override it
-            #     # So that whats' generated in n_body is in line with what is going to be sent.
-            #     # https://github.com/caronc/apprise/issues/633#issuecomment-1191449321
-            #     if not 'format=' in url and (n_format == 'Text' or n_format == 'Markdown'):
-            #         prefix = '?' if not '?' in url else '&'
-            #         # Apprise format is lowercase text https://github.com/caronc/apprise/issues/633
-            #         n_format = n_format.lower()
-            #         url = f"{url}{prefix}format={n_format}"
-            #     # If n_format == HTML, then apprise email should default to text/html and we should be sending HTML only
+            apobj.add(url)
 
-            # apobj.add(url)
+            sent_objs.append({'title': n_title,
+                              'body': n_body,
+                              'url': url,
+                              'body_format': n_format})
+            need_apprise = True
 
-            # sent_objs.append({'title': n_title,
-            #                   'body': n_body,
-            #                   'url': url,
-            #                   'body_format': n_format})
-
-        # # Blast off the notifications tht are set in .add()
-        # apobj.notify(
-        #     title=n_title,
-        #     body=n_body,
-        #     body_format=n_format,
-        #     # False is not an option for AppRise, must be type None
-        #     attach=n_object.get('screenshot', None)
-        # )
+        if need_apprise:
+            # Blast off the notifications tht are set in .add()
+            apobj.notify(
+                title=n_title,
+                body=n_body,
+                body_format=n_format,
+                # False is not an option for AppRise, must be type None
+                attach=n_object.get('screenshot', None)
+            )
 
 
-        # Returns empty string if nothing found, multi-line string otherwise
-        log_value = logs.getvalue()
+            # Returns empty string if nothing found, multi-line string otherwise
+            log_value = logs.getvalue()
 
-        if log_value and 'WARNING' in log_value or 'ERROR' in log_value:
-            logger.critical(log_value)
-            raise Exception(log_value)
+            if log_value and 'WARNING' in log_value or 'ERROR' in log_value:
+                logger.critical(log_value)
+                raise Exception(log_value)
 
     # Return what was sent for better logging - after the for loop
     return sent_objs
